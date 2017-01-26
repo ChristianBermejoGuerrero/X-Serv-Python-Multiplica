e99556990310a48455188110aecfe40f99ebdaa1
ChristianBermejo <cbermejog7gmail.com>
2017-01-26 15:51:47 +0100

    Tablas de Multiplicar HECHO

---
+++ Multiplica.py
@@ -0,0 +1,9 @@
+# Christian Bermejo Guerrero
+# 13.3 Tablas de Multiplicar
+
+for i in range(1,11):
+    print()
+    print("Tabla del",i)
+    print("------------")
+    for j in range(1,11):
+        print(i,"por",j,"es",i*j)
